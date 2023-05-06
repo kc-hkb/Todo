2e53feabff0837f3f62c0f823784f0a645a0adf9
kc-hkb <7kei54egc@gmail.com>
2023-05-06 17:05:58 +0900

    記事の投稿機能及び編集機能の追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,38 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to root_path,notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存できませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+	end
+
+  def update
+    @board = Board.find(params[:id])
+		if @board.update(board_params)
+			redirect_to root_path,
+			notice: '更新できました。'
+		else
+			flash.now[:error] = '更新できませんでした。'
+			render :edit
+		end
+	end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
 end
