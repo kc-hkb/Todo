4286bf5c0202d701da6c9d57c7b4d8cc12f934f5
kc-hkb <7kei54egc@gmail.com>
2023-05-07 09:40:55 +0900

    ユーザーとボードの紐づけを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,10 @@
 class Board < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  belongs_to :user
+
+  def author_name
+    user.display_name
+  end
 end
