72e905e9aa7930d283b936f74e1f574a7e389a50
kc-hkb <7kei54egc@gmail.com>
2023-05-07 15:57:56 +0900

    タスク追加機能作成（途中）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :date, presence: true
+
+  belongs_to :user
+  belongs_to :board
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'boards#index'
 
-  resources :boards, only: [:new, :create, :edit, :update, :destroy]
+  resources :boards, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :tasks, only: [:show, :new, :create, :edit, :update]
+  end
 end
